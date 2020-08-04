d7201068357f49a9c1a4a81f02227b0baf05d57a
Jake Martin <Jake.Martin@colorado.edu>
2020-08-04 15:41:57 -0600

    RNN now functioning, issues fixed by using python 3.85 and tf-nightly

--- kerasRNN.py
+++ kerasRNN.py
@@ -1,25 +1,59 @@
 # Sourced from the blog at https://victorzhou.com/blog/keras-rnn-tutorial/
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Don't print verbose info msgs
-#from tensorflow.keras.preprocessing import text_dataset_from_directory
-import tensorflow as tf
+from tensorflow.keras.preprocessing import text_dataset_from_directory
 from tensorflow.strings import regex_replace
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import Input
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+from tensorflow.keras.layers import Embedding, LSTM, Dense
 import numpy
 
 def prepareData(dir):
-    data = tf.keras.preprocessing.text_dataset_from_directory(dir)
+    data = text_dataset_from_directory(dir)
     return data.map(
         lambda text, label: (regex_replace(text, '<br />', ' '), label),
     )
 
-train_data = tf.keras.preprocessing.text_dataset_from_directory("./aclImdb/train")
-test_data = tf.keras.preprocessing.text_dataset_from_directory("./aclImdb/test")
+train_data = prepareData("./aclImdb/train")
+test_data = prepareData("./aclImdb/test")
 
-for text_batch, label_batch in train_data.take(1):
-    print(text_batch.numpy()[0])
-    print(label_batch.numpy()[0]) # 0 = neg, 1 = pos
-
+# Create sequential NN model, with string input layer
 model = Sequential()
-model.add(Input(shape=1,), dtype="string")
+model.add(Input(shape=(1,), dtype="string"))
+
+# Convert string input into sequence of token ints
+max_tokens = 1000
+max_len = 100
+vectorize_layer = TextVectorization(
+    # Max vocab size for tokens consider in-vocab
+    max_tokens=max_tokens,
+    # Output integer indices, one per string token
+    output_mode="int",
+    # Pad/truncate to always be exact length of tokens
+    output_sequence_length=max_len,
+)
+train_texts = train_data.map(lambda text, label: text)
+vectorize_layer.adapt(train_texts)
+model.add(vectorize_layer)
+
+# Convert ints to fixed-len vectors, vocab + 1 for out-of-vocab
+model.add(Embedding(max_tokens+1, 128))
+# Recurrent LSTM layer with 64 size output space
+model.add(LSTM(64))
+# Dense and output Layers
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy'],
+)
+
+# Train the model
+print("\nTraining model:")
+model.fit(train_data, epochs=10)
+
+model.save_weights('rnn.h5')
