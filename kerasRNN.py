53612149b7f57150ebfd386789aa7210a9012fc9
Jake Martin <Jake.Martin@colorado.edu>
2020-08-04 01:06:06 -0600

    Add gitignore and kerasRNN files

---
+++ kerasRNN.py
@@ -0,0 +1,5 @@
+from tensorflow.keras.preprocessing import text_dataset_from_directory
+
+train_data = text_dataset_from_directory("./aclImdb/train")
+test_data = text_dataset_from_directory("./aclImdb/test")
+asdfasdf
